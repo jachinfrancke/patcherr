6f22c847a600e3d4d25bc90504993c4f640e7f69
jachin <jachin@nedbase.nl>
2026-03-02 11:50:11 +0100

    Add PluginUpdateChecker WP-870

--- patcherr.php
+++ patcherr.php
@@ -2,3 +2,20 @@
 /**
  * Plugin Name: Patcherr
  */
+defined('ABSPATH') || exit;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$updateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/jachinfrancke/patcherr/',
+    __FILE__,
+    'patcherr'
+);
+
+$updateChecker->getVcsApi()->enableReleaseAssets();
+
+add_action('admin_notices', function () {
+    echo '<div class="notice notice-success is-dismissible"><p>Patcherr is loaded and ready to patch your site!</p></div>';
+});
